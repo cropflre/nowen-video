1bb28b39631035ae7c0d083856dad0aadd22d28e
Drug <cropflre2333@gmail.com>
2026-08-02 06:31:53 +0800

    test(transcode): verify presentation cadence ordering

---
+++ internal/transcode/certification/reorder_cadence_order_test.go
@@ -0,0 +1,32 @@
+package certification
+
+import "testing"
+
+func TestOrderCadencePointsForReorderEvidence(t *testing.T) {
+	points := []outputCadencePoint{
+		{Ticks: 0, Micros: 0},
+		{Ticks: 9_000, Micros: 100_000},
+		{Ticks: 3_000, Micros: 33_333},
+		{Ticks: 6_000, Micros: 66_667},
+	}
+	pts := []int64{0, 9_000, 3_000, 6_000}
+	orderedTicks, orderedPoints := orderCadencePointsForEvidence("reorder_case_candidate_run_01_startup", pts, points)
+	want := []int64{0, 3_000, 6_000, 9_000}
+	for index := range want {
+		if orderedTicks[index] != want[index] || orderedPoints[index].Ticks != want[index] {
+			t.Fatalf("presentation order at %d = ticks %d point %d, want %d", index, orderedTicks[index], orderedPoints[index].Ticks, want[index])
+		}
+	}
+	if pts[1] != 9_000 || points[1].Ticks != 9_000 {
+		t.Fatal("presentation ordering mutated packet-order inputs")
+	}
+}
+
+func TestOrderCadencePointsKeepsExistingEvidenceOrder(t *testing.T) {
+	points := []outputCadencePoint{{Ticks: 9_000}, {Ticks: 3_000}}
+	pts := []int64{9_000, 3_000}
+	orderedTicks, orderedPoints := orderCadencePointsForEvidence("output_startup", pts, points)
+	if orderedTicks[0] != 9_000 || orderedPoints[0].Ticks != 9_000 {
+		t.Fatal("existing cadence contract order changed")
+	}
+}
