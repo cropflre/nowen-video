13be8e5a0ec6389b54e29e52f54e212e414b7fc5
Drug <cropflre2333@gmail.com>
2026-08-02 06:43:16 +0800

    feat(transcode): bind perceptual reorder evidence

--- internal/transcode/reordercandidate/types.go
+++ internal/transcode/reordercandidate/types.go
@@ -35,52 +35,55 @@ type OffsetBucket struct {
 }
 
 type PacketOrderEvidence struct {
-	Kind                         string                              `json:"kind"`
-	TimeBase                     string                              `json:"time_base"`
-	PacketCount                  int                                 `json:"packet_count"`
-	FirstPTS                     int64                               `json:"first_pts"`
-	FirstDTS                     int64                               `json:"first_dts"`
-	LastPTS                      int64                               `json:"last_pts"`
-	LastDTS                      int64                               `json:"last_dts"`
-	FirstPTSMicros               int64                               `json:"first_pts_micros"`
-	FirstDTSMicros               int64                               `json:"first_dts_micros"`
-	LastPTSMicros                int64                               `json:"last_pts_micros"`
-	LastDTSMicros                int64                               `json:"last_dts_micros"`
-	MinCompositionOffsetTicks    int64                               `json:"min_composition_offset_ticks"`
-	MaxCompositionOffsetTicks    int64                               `json:"max_composition_offset_ticks"`
-	MinCompositionOffsetMicros   int64                               `json:"min_composition_offset_micros"`
-	MaxCompositionOffsetMicros   int64                               `json:"max_composition_offset_micros"`
-	ReorderedPacketCount         int                                 `json:"reordered_packet_count"`
-	PTSBeforeDTSCount            int                                 `json:"pts_before_dts_count"`
-	PTSAfterDTSCount             int                                 `json:"pts_after_dts_count"`
-	PTSEqualDTSCount             int                                 `json:"pts_equal_dts_count"`
-	AdjacentPTSInversionCount    int                                 `json:"adjacent_pts_inversion_count"`
-	DTSNonMonotonicCount         int                                 `json:"dts_non_monotonic_count"`
-	DTSDuplicateCount            int                                 `json:"dts_duplicate_count"`
-	MaxPresentationReorderDepth  int                                 `json:"max_presentation_reorder_depth"`
-	DTSDeltaHistogram            []transcodeoutputcadence.DeltaBucket `json:"dts_delta_histogram"`
-	CompositionOffsetHistogram   []OffsetBucket                      `json:"composition_offset_histogram"`
+	Kind                        string                               `json:"kind"`
+	TimeBase                    string                               `json:"time_base"`
+	PacketCount                 int                                  `json:"packet_count"`
+	FirstPTS                    int64                                `json:"first_pts"`
+	FirstDTS                    int64                                `json:"first_dts"`
+	LastPTS                     int64                                `json:"last_pts"`
+	LastDTS                     int64                                `json:"last_dts"`
+	FirstPTSMicros              int64                                `json:"first_pts_micros"`
+	FirstDTSMicros              int64                                `json:"first_dts_micros"`
+	LastPTSMicros               int64                                `json:"last_pts_micros"`
+	LastDTSMicros               int64                                `json:"last_dts_micros"`
+	MinCompositionOffsetTicks   int64                                `json:"min_composition_offset_ticks"`
+	MaxCompositionOffsetTicks   int64                                `json:"max_composition_offset_ticks"`
+	MinCompositionOffsetMicros  int64                                `json:"min_composition_offset_micros"`
+	MaxCompositionOffsetMicros  int64                                `json:"max_composition_offset_micros"`
+	ReorderedPacketCount        int                                  `json:"reordered_packet_count"`
+	PTSBeforeDTSCount           int                                  `json:"pts_before_dts_count"`
+	PTSAfterDTSCount            int                                  `json:"pts_after_dts_count"`
+	PTSEqualDTSCount            int                                  `json:"pts_equal_dts_count"`
+	AdjacentPTSInversionCount   int                                  `json:"adjacent_pts_inversion_count"`
+	DTSNonMonotonicCount        int                                  `json:"dts_non_monotonic_count"`
+	DTSDuplicateCount           int                                  `json:"dts_duplicate_count"`
+	MaxPresentationReorderDepth int                                  `json:"max_presentation_reorder_depth"`
+	DTSDeltaHistogram           []transcodeoutputcadence.DeltaBucket `json:"dts_delta_histogram"`
+	CompositionOffsetHistogram  []OffsetBucket                       `json:"composition_offset_histogram"`
 }
 
 type RunEvidence struct {
-	Ordinal                 int                            `json:"ordinal"`
-	Base                    transcodetimebase.RunEvidence  `json:"base"`
-	StartupPacketOrder      PacketOrderEvidence            `json:"startup_packet_order"`
-	ContinuationPacketOrder PacketOrderEvidence            `json:"continuation_packet_order"`
+	Ordinal                         int                             `json:"ordinal"`
+	Base                            transcodetimebase.RunEvidence   `json:"base"`
+	StartupPacketOrder              PacketOrderEvidence             `json:"startup_packet_order"`
+	ContinuationPacketOrder         PacketOrderEvidence             `json:"continuation_packet_order"`
+	StartupPerceptualSequence       PerceptualFrameSequence         `json:"startup_perceptual_sequence"`
+	ContinuationPerceptualSequence  PerceptualFrameSequence         `json:"continuation_perceptual_sequence"`
 }
 
 type CandidateSummary struct {
-	Base                              transcodetimebase.CandidateSummary `json:"base"`
-	StartupReorderedPacketCount       transcodetimebase.MetricRange      `json:"startup_reordered_packet_count"`
-	ContinuationReorderedPacketCount  transcodetimebase.MetricRange      `json:"continuation_reordered_packet_count"`
-	StartupMaxReorderDepth            transcodetimebase.MetricRange      `json:"startup_max_reorder_depth"`
-	ContinuationMaxReorderDepth       transcodetimebase.MetricRange      `json:"continuation_max_reorder_depth"`
-	StartupMaxCompositionOffsetMicros transcodetimebase.MetricRange      `json:"startup_max_composition_offset_micros"`
-	ContinuationMaxCompositionOffsetMicros transcodetimebase.MetricRange `json:"continuation_max_composition_offset_micros"`
-	PacketOrderStable                 bool                               `json:"packet_order_stable"`
-	StrictDTS                         bool                               `json:"strict_dts"`
-	ReorderObserved                   bool                               `json:"reorder_observed"`
-	Stable                            bool                               `json:"stable"`
+	Base                                   transcodetimebase.CandidateSummary `json:"base"`
+	StartupReorderedPacketCount            transcodetimebase.MetricRange      `json:"startup_reordered_packet_count"`
+	ContinuationReorderedPacketCount       transcodetimebase.MetricRange      `json:"continuation_reordered_packet_count"`
+	StartupMaxReorderDepth                 transcodetimebase.MetricRange      `json:"startup_max_reorder_depth"`
+	ContinuationMaxReorderDepth            transcodetimebase.MetricRange      `json:"continuation_max_reorder_depth"`
+	StartupMaxCompositionOffsetMicros      transcodetimebase.MetricRange      `json:"startup_max_composition_offset_micros"`
+	ContinuationMaxCompositionOffsetMicros transcodetimebase.MetricRange      `json:"continuation_max_composition_offset_micros"`
+	PacketOrderStable                      bool                               `json:"packet_order_stable"`
+	PerceptualSequenceStable               bool                               `json:"perceptual_sequence_stable"`
+	StrictDTS                              bool                               `json:"strict_dts"`
+	ReorderObserved                        bool                               `json:"reorder_observed"`
+	Stable                                 bool                               `json:"stable"`
 }
 
 type CandidateEvidence struct {
@@ -90,29 +93,33 @@ type CandidateEvidence struct {
 }
 
 type CandidateComparison struct {
-	Base                        transcodetimebase.CandidateComparison `json:"base"`
-	StartupPacketOrderEquivalent bool                                `json:"startup_packet_order_equivalent"`
-	ContinuationPacketOrderEquivalent bool                           `json:"continuation_packet_order_equivalent"`
-	Equivalent                  bool                                  `json:"equivalent"`
+	Base                              transcodetimebase.CandidateComparison `json:"base"`
+	SemanticBaseEquivalent            bool                                  `json:"semantic_base_equivalent"`
+	StartupPerceptualComparison       PerceptualFrameComparison             `json:"startup_perceptual_comparison"`
+	ContinuationPerceptualComparison  PerceptualFrameComparison             `json:"continuation_perceptual_comparison"`
+	StartupPacketOrderEquivalent      bool                                  `json:"startup_packet_order_equivalent"`
+	ContinuationPacketOrderEquivalent bool                                  `json:"continuation_packet_order_equivalent"`
+	Equivalent                        bool                                  `json:"equivalent"`
 }
 
 type CaseEvidence struct {
-	Case                       CaseSpec                                  `json:"case"`
-	SourceStartupTimeline      transcodeoutputcadence.TimelineEvidence   `json:"source_startup_timeline"`
-	SourceContinuationTimeline transcodeoutputcadence.TimelineEvidence   `json:"source_continuation_timeline"`
-	Candidates                 []CandidateEvidence                       `json:"candidates"`
-	Comparison                 CandidateComparison                       `json:"comparison"`
+	Case                       CaseSpec                                `json:"case"`
+	SourceStartupTimeline      transcodeoutputcadence.TimelineEvidence `json:"source_startup_timeline"`
+	SourceContinuationTimeline transcodeoutputcadence.TimelineEvidence `json:"source_continuation_timeline"`
+	Candidates                 []CandidateEvidence                     `json:"candidates"`
+	Comparison                 CandidateComparison                     `json:"comparison"`
 }
 
 type Contract struct {
-	SchemaVersion         string         `json:"schema_version"`
-	FFmpegVersion         string         `json:"ffmpeg_version"`
-	FFprobeVersion        string         `json:"ffprobe_version"`
-	RepeatCount           int            `json:"repeat_count"`
-	PacketVarianceTicks   int64          `json:"packet_variance_ticks"`
-	Cases                 []CaseEvidence `json:"cases"`
-	SeamlessAllowed       bool           `json:"seamless_allowed"`
-	DiscontinuityRequired bool           `json:"discontinuity_required"`
+	SchemaVersion                string         `json:"schema_version"`
+	FFmpegVersion                string         `json:"ffmpeg_version"`
+	FFprobeVersion               string         `json:"ffprobe_version"`
+	RepeatCount                  int            `json:"repeat_count"`
+	PacketVarianceTicks          int64          `json:"packet_variance_ticks"`
+	PerceptualMaxHammingDistance int            `json:"perceptual_max_hamming_distance"`
+	Cases                        []CaseEvidence `json:"cases"`
+	SeamlessAllowed              bool           `json:"seamless_allowed"`
+	DiscontinuityRequired        bool           `json:"discontinuity_required"`
 }
 
 func (s CaseSpec) Validate() error {
@@ -141,7 +148,7 @@ func (c Contract) ValidateIdentity() error {
 	if strings.TrimSpace(c.FFmpegVersion) == "" || strings.TrimSpace(c.FFprobeVersion) == "" {
 		return fmt.Errorf("toolchain identity is incomplete")
 	}
-	if c.RepeatCount != RepeatCount || c.PacketVarianceTicks != PacketVarianceTolerance {
+	if c.RepeatCount != RepeatCount || c.PacketVarianceTicks != PacketVarianceTolerance || c.PerceptualMaxHammingDistance != PerceptualMaxHammingDistance {
 		return fmt.Errorf("reorder variance policy is invalid")
 	}
 	if c.SeamlessAllowed || !c.DiscontinuityRequired {
