058e1d83ead695261316e053d42dec28b7c7df37
Drug <cropflre2333@gmail.com>
2026-08-02 14:21:31 +0800

    feat(transcode): add long-duration HLS command

---
+++ internal/transcode/certification/long_duration_drift_command.go
@@ -0,0 +1,102 @@
+package certification
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	serviceffmpeg "github.com/nowen-video/nowen-video/internal/service/ffmpeg"
+	transcodelongdrift "github.com/nowen-video/nowen-video/internal/transcode/longdrift"
+	transcodereorder "github.com/nowen-video/nowen-video/internal/transcode/reordercandidate"
+	transcodetimebase "github.com/nowen-video/nowen-video/internal/transcode/timebasecandidate"
+	transcodetimestamp "github.com/nowen-video/nowen-video/internal/transcode/timestampplan"
+)
+
+func produceLongDurationDriftCandidate(
+	ctx context.Context,
+	ffmpegPath,
+	outputDir,
+	sourcePath string,
+	timestampPlan transcodetimestamp.Plan,
+	caseSpec transcodereorder.CaseSpec,
+	candidateSpec transcodetimebase.CandidateSpec,
+) (encoderTimeBaseProduced, error) {
+	if err := os.MkdirAll(outputDir, 0o755); err != nil {
+		return encoderTimeBaseProduced{}, err
+	}
+	args, err := longDurationDriftHLSArgs(sourcePath, outputDir, timestampPlan, caseSpec, candidateSpec)
+	if err != nil {
+		return encoderTimeBaseProduced{}, err
+	}
+	if err := runCommand(ctx, ffmpegPath, args...); err != nil {
+		return encoderTimeBaseProduced{}, err
+	}
+	return encoderTimeBaseProduced{Manifest: filepath.Join(outputDir, "stream.m3u8"), Args: args}, nil
+}
+
+func longDurationDriftHLSArgs(
+	sourcePath,
+	outputDir string,
+	timestampPlan transcodetimestamp.Plan,
+	caseSpec transcodereorder.CaseSpec,
+	candidateSpec transcodetimebase.CandidateSpec,
+) ([]string, error) {
+	args := serviceffmpeg.BuildHLSArgs(serviceffmpeg.BuildOptions{
+		InputPath: sourcePath,
+		OutputDir: outputDir,
+		HWAccel: serviceffmpeg.HWAccelNone,
+		Profile: serviceffmpeg.Profile{
+			Width: fixtureWidth,
+			Height: fixtureHeight,
+			VideoBitrate: "800k",
+			AudioBitrate: "128k",
+			MaxBitrate: "900k",
+			BufSize: "1600k",
+		},
+		X264Preset: "veryfast",
+		SoftwareTune: VideoTuneZeroLatency,
+		Threads: 1,
+		UseCRF: true,
+		CRF: 23,
+		VideoFilter: fmt.Sprintf("scale=%d:%d", fixtureWidth, fixtureHeight),
+		HLSTime: fixtureSegmentSeconds,
+		HLSFlags: "independent_segments+append_list+program_date_time",
+		HLSPlaylistType: "event",
+		StartNumber: 0,
+		ForceKeyFrames: true,
+		GOPSize: caseSpec.Base.GOPSize,
+	})
+	args = insertBeforeInput(args, sourcePath, "-stream_loop", "-1")
+	var err error
+	args, err = asBoundedStartupVODMicros(args, transcodelongdrift.DurationMicros)
+	if err != nil {
+		return nil, err
+	}
+	args, err = transcodetimestamp.ApplyFFmpeg(args, timestampPlan)
+	if err != nil {
+		return nil, err
+	}
+	args = removeReorderOptionPair(args, "-tune", VideoTuneZeroLatency)
+	x264Params := fmt.Sprintf("b-adapt=%d:b-pyramid=none:open-gop=0:scenecut=0", caseSpec.BAdapt)
+	return insertIsolationBeforeOutput(args,
+		"-enc_time_base:v:0", candidateSpec.EncoderTimeBase,
+		"-bf", fmt.Sprint(caseSpec.BFrames),
+		"-b_strategy", fmt.Sprint(caseSpec.BAdapt),
+		"-refs", fmt.Sprint(caseSpec.ReferenceFrames),
+		"-x264-params", x264Params,
+	), nil
+}
+
+func insertBeforeInput(args []string, sourcePath string, values ...string) []string {
+	for index := 0; index+1 < len(args); index++ {
+		if args[index] == "-i" && args[index+1] == sourcePath {
+			result := make([]string, 0, len(args)+len(values))
+			result = append(result, args[:index]...)
+			result = append(result, values...)
+			result = append(result, args[index:]...)
+			return result
+		}
+	}
+	return append([]string(nil), args...)
+}
