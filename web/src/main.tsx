02954c78abe4e4c121bd6177633de302b09681b5
Drug <cropflre2333@gmail.com>
2026-08-14 23:44:09 +0800

    fix(media): load detail shell flush override

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -23,6 +23,7 @@ import './styles/modern-cinema.css'
 import './styles/modern-cinema-reference.css'
 import './styles/modern-cinema-light-fix.css'
 import './styles/media-detail-cinema.css'
+import './styles/media-detail-shell-fix.css'
 
 const SW_DEV_RELOAD_KEY = 'nowen-sw-dev-cleanup-reload'
 const SW_UPDATE_RELOAD_KEY = 'nowen-sw-production-update-reload'
