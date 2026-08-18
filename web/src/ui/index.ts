9b159548d8da2d1cb38efe65f8876b179a7c461b
Drug <cropflre2333@gmail.com>
2026-08-18 22:14:24 +0800

    汇总导出第二阶段统一组件

--- web/src/ui/index.ts
+++ web/src/ui/index.ts
@@ -1,2 +1,11 @@
 export { MediaArtwork, type MediaArtworkProps, type MediaArtworkRatio } from './media/MediaArtwork'
 export { MediaRail, type MediaRailProps } from './media/MediaRail'
+export { MediaHeroContent, type MediaHeroContentProps } from './media/MediaHeroContent'
+export { AppShell, type AppShellProps } from './layout/AppShell'
+export { PageHeader, type PageHeaderProps } from './layout/PageHeader'
+export { MediaGrid, type MediaGridProps, type MediaGridVariant } from './layout/MediaGrid'
+export {
+  BottomNavigation,
+  type BottomNavigationProps,
+  type BottomNavigationItem,
+} from './navigation/BottomNavigation'
